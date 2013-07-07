6a91b72687f934a3b6229fe9e45ff2895871bd41
Alexis Bellido <alexis@ventanazul.com>
2013-07-06 21:18:50 -0400

    use objects from json file

--- nodejs/code/ch3/blog_recent.js
+++ nodejs/code/ch3/blog_recent.js
@@ -3,13 +3,23 @@ var fs = require('fs');
 
 http.createServer(function(req, res) {
   if (req.url == '/') {
-    fs.readFile('./titles.json', function(err, data) {
-      if (err) { <co id="blog_recent_error"/>
+    // this used a normal array
+    //fs.readFile('./titles.json', function(err, data) {
+    // but this uses an array of objects
+    fs.readFile('./entries.json', function(err, data) {
+      if (err) {
         console.error(err);
         res.end('Server Error');
       }
       else {
-        var titles = JSON.parse(data.toString());
+        // this used a normal array
+        //var titles = JSON.parse(data.toString());
+        // but this uses an array of objects
+        var titles = []
+        var entries = JSON.parse(data.toString());
+        for (var i = 0; i < entries.length; i++) {
+            titles.push(entries[i].title);
+        }
 
         fs.readFile('./template.html', function(err, data) {
           if (err) {
@@ -18,8 +28,7 @@ http.createServer(function(req, res) {
           }
           else {
             var tmpl = data.toString();
-
-            var html = tmpl.replace('%', titles.join('&lt;/li&gt;&lt;li&gt;'));
+            var html = tmpl.replace('%', titles.join('</li><li>'));
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.end(html);
           }
