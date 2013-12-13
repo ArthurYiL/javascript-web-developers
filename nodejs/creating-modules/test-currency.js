91b8bc533152e9023ca7f4dcb48810c1ddc04ae1
Alexis Bellido <alexis@ventanazul.com>
2013-12-13 19:51:53 +0000

    require test for one file and one subdirectory under the file making the call

--- nodejs/creating-modules/test-currency.js
+++ nodejs/creating-modules/test-currency.js
@@ -1,4 +1,11 @@
+// this uses a file in the same directory
 var currency = require('./currency');
+
+// and this uses a subdirectory, containing an index.js file, in the same directory
+//var currency = require('./my_module');
 
 console.log('50 canadian dollars is US dollars:');
 console.log(currency.canadianToUS(50));
+
+console.log('30 US dollars is Canadian dollars:');
+console.log(currency.USToCanadian(30));
