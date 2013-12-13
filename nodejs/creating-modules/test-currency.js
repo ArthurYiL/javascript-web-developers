1071322a19a6dbeb046837dee05d815cbdaddf20
Alexis Bellido <alexis@ventanazul.com>
2013-12-13 21:21:42 +0000

    use directory for module with package.json pointing to file different than index.js and use node_modules under current directory

--- nodejs/creating-modules/test-currency.js
+++ nodejs/creating-modules/test-currency.js
@@ -1,8 +1,14 @@
 // this uses a file in the same directory
-var currency = require('./currency');
+//var currency = require('./currency');
 
 // and this uses a subdirectory, containing an index.js file, in the same directory
 //var currency = require('./my_module');
+
+// using directory with no index.js and package.json
+//var currency = require('./dir_no_index');
+
+// use node_modules under current directory
+var currency = require('currency');
 
 console.log('50 canadian dollars is US dollars:');
 console.log(currency.canadianToUS(50));
