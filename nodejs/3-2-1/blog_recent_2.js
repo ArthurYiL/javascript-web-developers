ff03351f776fe1126079adbd35c310e281f77f8e
Alexis Bellido <alexis@ventanazul.com>
2013-12-14 04:54:56 +0000

    basic callback example

---
+++ nodejs/3-2-1/blog_recent_2.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+  if (req.url == '/') {
+    getTitles('home', res);
+  }
+  if (req.url == '/test') {
+    getTitles('test', res);
+  }
+}).listen(3000);
+
+function getTitles(page, res) {
+  fs.readFile('./titles.json', function(err, data) {
+    if (err) {
+      hadError(err, res);
+    } else {
+      getTemplate(JSON.parse(data.toString()), page, res);
+    }
+  });
+}
+
+function getTemplate(titles, page, res) {
+  fs.readFile('./template.html', function(err, data) {
+    if (err) {
+      hadError(err, res);
+    } else {
+      formatHtml(titles, data.toString(), page, res);
+    }
+  });
+}
+
+function formatHtml(titles, tmpl, page, res) {
+  var html = tmpl.replace('%', titles.join('</li><li>'));
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<h1>Refactor with named functions. Page: ' + page + '</h1>');
+  res.end(html);
+}
+
+function hadError(err, res) {
+  console.error(err);
+  res.end('Server Error');
+}
