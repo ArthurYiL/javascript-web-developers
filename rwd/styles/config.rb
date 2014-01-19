f92a089a69ec25643eab02a437a3b7a76023ad41
Alexis Bellido <alexis@ventanazul.com>
2014-01-19 13:48:52 -0500

    add basic responsive tests, css only

---
+++ rwd/styles/config.rb
@@ -0,0 +1,15 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/sites/all/themes/capitalny/capitalny"
+css_dir = "css"
+fonts_dir = css_dir + "/" + "fonts"
+sass_dir = "scss"
+images_dir = "images"
+javascripts_dir = "javascripts"
+generated_images_dir = "sprites"
+http_generated_images_path = http_path + "/" + generated_images_dir
+#output_style = :compressed
+output_style = :expanded
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+line_comments = false
