c7526521448c40526c227e056b24958e8a9a1ee5
Alexis Bellido <alexis@ventanazul.com>
2014-08-12 17:28:15 -0400

    specify main file for module in directory

--- basic-objects/constructor-prototype.js
+++ basic-objects/constructor-prototype.js
@@ -13,6 +13,7 @@ var person7 = new Person("Nick", 30, "Architect");
 var person8 = new Person("Pete", 22, "Doctor");
 
 person7.friends.push("Mick");
+person7.friends.push("Tim");
 console.log(person7.friends);
 console.log(person8.friends);
 
--- nodejs/creating-modules/test-currency.js
+++ nodejs/creating-modules/test-currency.js
@@ -2,13 +2,13 @@
 //var currency = require('./currency');
 
 // and this uses a subdirectory, containing an index.js file, in the same directory
-//var currency = require('./my_module');
+var currency = require('./my_module');
 
 // using directory with no index.js and package.json
 //var currency = require('./dir_no_index');
 
 // use node_modules under current directory
-var currency = require('currency');
+//var currency = require('currency');
 
 console.log('50 canadian dollars is US dollars:');
 console.log(currency.canadianToUS(50));
