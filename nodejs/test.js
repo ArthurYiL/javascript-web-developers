da1af62f462122dcbe89390a81d4cd569c345733
Alexis Bellido <alexis@ventanazul.com>
2014-08-26 22:33:32 -0400

    create basic mymodule

--- nodejs/test.js
+++ nodejs/test.js
@@ -1,2 +1,7 @@
+var mymodule = require('./mymodule');
+
 var num = 4;
 console.log("This is a test with number: " + num);
+
+console.log("mymodule.num: " + mymodule.num);
+console.log("Add numbers with mymodule.sum: " + mymodule.sum(3, 5));
