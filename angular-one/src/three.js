50fde107739e4a826f89f3b14cafd751728e5e10
Alexis Bellido <alexis@ventanazul.com>
2014-09-04 12:23:47 -0400

    start three

---
+++ angular-one/src/three.js
@@ -0,0 +1,10 @@
+console.log('two here');
+
+function SimpleController($scope) {
+  $scope.customers = [
+    {name: 'Yuk Michaels', city: 'Phoenix'}, 
+    {name: 'Elijah Smith', city: 'New York'}, 
+    {name: 'Jane Doe', city: 'San Francisco'}
+  ];
+}
+
