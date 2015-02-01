ef38921879b5457b7c2172c8539af740e5ee6567
Alexis Bellido <alexis@ventanazul.com>
2015-02-01 15:43:11 -0500

    add stories to angello

--- angello/app.js
+++ angello/app.js
@@ -1,1 +1,16 @@
-console.log('angello');
+var myModule = angular.module('Angello', []);
+
+myModule.controller('MainCtrl', function($scope) {
+  $scope.setCurrentStory = function(story) {
+    $scope.currentStory = story
+  };
+
+  $scope.stories = [
+    {title: 'Story 00', description: 'Description pending 00.'},
+    {title: 'Story 01', description: 'Description pending 01.'},
+    {title: 'Story 02', description: 'Description pending 02.'},
+    {title: 'Story 03', description: 'Description pending 03.'},
+    {title: 'Story 04', description: 'Description pending 04.'},
+    {title: 'Story 05', description: 'Description pending 05.'}
+  ];
+});
