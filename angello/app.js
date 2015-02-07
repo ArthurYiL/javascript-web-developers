f21c3a58426b7e1aff6ebd004b55d15e05e149be
Alexis Bellido <alexis@ventanazul.com>
2015-02-07 12:45:11 -0500

    first use of ng-options

--- angello/app.js
+++ angello/app.js
@@ -27,12 +27,37 @@ myModule.controller('MainCtrl', function($scope) {
   };
 
   $scope.stories = [
-    {title: 'Story Zero', description: 'Description pending 00.'},
+    {
+      title: 'Story Zero', 
+      description: 'Description pending 00.',
+      criteria: 'Criteria pending 00',
+      status: 'To do',
+      type: 'Feature',
+      reporter: 'Luke Skywalker',
+      assignee: 'Brian Ford'
+    },
     {title: 'Story 01', description: 'Description pending 01.'},
     {title: 'Story 02', description: 'Description pending 02.'},
     {title: 'Story 03', description: 'Description pending 03.'},
     {title: 'Story 04', description: 'Description pending 04.'},
     {title: 'Story Five', description: 'Description pending 05.'}
+  ];
+
+  $scope.statuses = [
+    {name:'Back Log'},
+    {name:'To Do'},
+    {name:'In Progress'},
+    {name:'Code Review'},
+    {name:'QA Review'},
+    {name:'Verified'},
+    {name:'Done'}
+  ];
+
+  $scope.types = [
+    {name:'Feature'},
+    {name:'Enhancement'},
+    {name:'Bug'},
+    {name:'Spike'}
   ];
 
 });
