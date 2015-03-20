dba01b4228de8bf487df23fe0a12b8ac0cccfc78
Alexis Bellido <alexis@ventanazul.com>
2015-03-20 10:21:21 -0400

    $scope and $apply

--- angello/hello.js
+++ angello/hello.js
@@ -1,1 +1,26 @@
-console.log('hello monkey');
+var app = angular.module('Hello', []);
+
+function MyController($scope) {
+  // Disabling for testing but note how $scope.$apply is needed inside setInterval.
+  // This is because, if the callback executes outside of the angular context,
+  // we can force the $scope to have knowledge of the change using the $apply method.
+  //$scope.clock = {
+  //  now: new Date()
+  //};
+  //var updateClock = function() {
+  //  $scope.clock.now = new Date();
+  //};
+  //setInterval(function() {
+  //  $scope.$apply(updateClock);
+  //}, 1000);
+  //updateClock();
+}
+
+app.controller('MyController', MyController);
+
+app.controller('MySecondController', 
+  ['$scope', function($scope) {
+    $scope.movie = "Duck Tails";
+  }]);
+
+console.log('hello monkey business');
