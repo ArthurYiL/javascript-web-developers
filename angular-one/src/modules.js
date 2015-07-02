b2a1d39bb2c44794f44e0ffd1a073df55a26ef0d
Alexis Bellido <alexis@ventanazul.com>
2015-07-02 13:57:25 -0400

    some basic jasmine tests

--- angular-one/src/modules.js
+++ angular-one/src/modules.js
@@ -1,1 +1,28 @@
-var app = angular.module('myApp', []);
+var myAppModule = angular.module('myApp', []);
+
+myAppModule.run(function () {
+  // Some jasmine inside this angular controller
+  describe("Some angular stuff", function() {
+    it("says hi", function() {
+      expect(helloWorld()).toEqual("hello");
+    });
+  });
+});
+
+myAppModule.filter('greet', function() {
+  return function(name) {
+    return 'Hello, ' + name + '!';
+  }
+});
+
+// A global function and test with jasmine
+
+function helloWorld() {
+  return 'hello';
+}
+
+describe("Hello World", function() {
+  it("says hello", function() {
+    expect(helloWorld()).toEqual("hello");
+  });
+});
