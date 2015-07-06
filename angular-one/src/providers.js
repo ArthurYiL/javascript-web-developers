3b6ef9665d5980495a3e0dd576891e352c646b78
Alexis Bellido <alexis@ventanazul.com>
2015-07-06 12:41:30 -0400

    basic value recipe and controller-as syntax explanation

--- angular-one/src/providers.js
+++ angular-one/src/providers.js
@@ -4,6 +4,19 @@ var myApp = angular.module('myApp', []);
 // inject to controller using array
 // see: https://docs.angularjs.org/guide/di 
 
+// Value recipe
+myApp.value('clientId', 'a123');
+
 myApp.controller('MyController', ['$scope', function($scope) {
+  // This is using $scope, so the directive is a simple ng-controller="MyController".
   $scope.color = 'green';
+}]);
+
+myApp.controller('DemoController', ['clientId', function DemoController(clientId) {
+  // Note we're using inline annotation too to inject dependencies.
+  
+  // Below, we try another way of doing "this.clientId = clientId;"
+  // which creates consistency with the directive ng-controller="DemoController as demo".
+  var demo = this;
+  demo.clientId = clientId;
 }]);
