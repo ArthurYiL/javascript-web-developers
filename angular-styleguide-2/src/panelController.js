e8fb9e4080b700544863c00f64e8e1836162f059
Alexis Bellido <alexis@ventanazul.com>
2015-07-10 14:56:55 -0400

    a simple method to create a story from a controller

--- angular-styleguide-2/src/panelController.js
+++ angular-styleguide-2/src/panelController.js
@@ -12,6 +12,13 @@
     var vm = this;
     vm.promoCode = 'promo code for ' + clientId;
 
+    // Prefer named functions and bind them at the top
+    // of the controller. The function is defined later via 
+    // a function declaration, hence it's hoisted.
+    vm.logCurrentStory = logCurrentStory;
+
+    vm.createStory = createStory;
+
     vm.stories = [
       {
         title: 'First story',
@@ -33,16 +40,27 @@
       }
     ];
 
+    // Avoid anonymous functions like this
     vm.setCurrentStory = function(story) {
       console.log('setCurrentStory');
       console.log(story);
     };
 
-    vm.logCurrentStory = logCurrentStory;
-
     function logCurrentStory(story) {
       console.log('logCurrentStory');
       console.log(story);
+    }
+
+    function createStory() {
+      vm.stories.push({
+        title: 'New story',
+        description: 'New Story',
+        criteria: 'Criteria pending.',
+        status: 'Back Log',
+        type: 'Feature',
+        reporter: 'Pending',
+        assignee: 'Pending'
+      });
     }
 
   }
