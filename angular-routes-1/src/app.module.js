0a6ae2d63a1ae9b7171dd0f1d324d74fd1d72b9f
Alexis Bellido <alexis@ventanazul.com>
2015-07-27 19:07:00 -0400

    preparing for basic routes with angular-route

--- angular-routes-1/src/app.module.js
+++ angular-routes-1/src/app.module.js
@@ -2,6 +2,19 @@
   "use strict";
 
   angular
-    .module('app', []);
+    .module('app', ['ngRoute'])
+    .config(['$routeProvider', function($routeProvider) {
+      console.log('routes with inline annotation');
+    }]);
+
+  //angular
+  //  .module('app', ['ngRoute'])
+  //  .config(setupRoutes);
+
+  //setupRoutes.$inject = ['$routeProvider'];
+
+  //function setupRoutes($routeProvider) {
+  //  console.log('route withs $inject');
+  //}
 
 })();
---
+++ angular-skeleton/src/panelController.js
@@ -0,0 +1,49 @@
+(function() {
+  "use strict";
+
+  angular
+    .module('app')
+    .controller('PanelController', Panel);
+
+  Panel.$inject = ['clientId', 'storyModel', 'helperFactory', '$timeout'];
+  
+  function Panel(clientId, storyModel, helperFactory, $timeout) {
+    /*jshint validthis: true */
+    var vm = this;
+
+    // Prefer named functions and bind them at the top
+    // of the controller. The function is defined later via 
+    // a function declaration, hence it's hoisted.
+    vm.logCurrentStory = logCurrentStory;
+
+    vm.createStory = createStory;
+    vm.stories = storyModel.getStories();
+    vm.sum = helperFactory.sum(3, 5);
+    vm.multiplication = helperFactory.multiply(3, 5);
+
+    // Avoid anonymous functions like this
+    vm.setCurrentStory = function(story) {
+      console.log('setCurrentStory');
+      console.log(story);
+    };
+
+    function logCurrentStory(story) {
+      console.log('logCurrentStory');
+      console.log(story);
+    }
+
+    function createStory() {
+      vm.stories.push({
+        title: 'New story',
+        description: 'New Story',
+        criteria: 'Criteria pending.',
+        status: 'Back Log',
+        type: 'Feature',
+        reporter: 'Pending',
+        assignee: 'Pending'
+      });
+    }
+
+  }
+
+})();
